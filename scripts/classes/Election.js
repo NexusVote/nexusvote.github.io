bd8459abe79c2d97b483dacce87af22f3bfb760a
RocketGirl62 <80389474+RocketGirl62@users.noreply.github.com>
2024-02-24 21:30:11 +0100

    Clean Up

--- scripts/classes/Election.js
+++ scripts/classes/Election.js
@@ -1,9 +1,6 @@
 class Election{
     constructor(profiles, kindOfSubjects, taskfields, subjects){
         
-        //Wozu brauche ich die Anzahlen? es reichen doch Arrays
-        //Eigentlich brauche ich hiervon nichts mehr
-        //Aber sonst, sind es jetz nur noch Arrays
         //Anzahl Profile
         this.profiles = profiles;
 
@@ -16,14 +13,6 @@ class Election{
         //Anzahl Fächer
         this.subjects = subjects;
 
-        //Sollte ich auflisten, welche Fächer etc. es gibt? brauch ich nicht, oder?
-        //Das hier brauche ich doch nicht
-        /*
-        //Gibt es eA Bänder
-        this.eABaender = eABaender;
-
-        //Gibt es gA Bänder
-        this.gABaender = gABaender;*/
 
     }
 
